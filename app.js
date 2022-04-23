bbe5a550803fee8e40b940e924a41738ece5da8c
rasitdeveloper <frasitkilic@gmail.com>
2022-04-23 10:51:00 +0300

    add ejs

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 const express = require('express');
 const app = express();
+
+//template engine
+app.set("view engine", "ejs");
+
+//middlewares
+app.use(express.static("public"));
+
+//routes
+app.get('/', (req, res) => {
+  res.status(200).render('index', {
+      page_name: "index"
+  });
+});
+app.get('/about', (req, res) => {
+    res.status(200).render('about', {
+        page_name: "about"
+    });
+  });
+
 const port = 3000;
 app.listen(port, () => {
-    console.log(`App started on port ${port}`)
+  console.log(`App started on port ${port}`);
 });
