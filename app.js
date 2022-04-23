ee944cff4aefea015a693b509dec262e40cc245d
rasitdeveloper <frasitkilic@gmail.com>
2022-04-23 14:18:09 +0300

    add course routes

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
 const express = require('express');
 const pageRoute = require('./routes/pageRoute');
+const mongoose = require('mongoose');
+const courseRoute = require('./routes/courseRoute');
+const { use } = require('./routes/pageRoute');
 
 const app = express();
+
+//connect db
+mongoose.connect('mongodb://localhost/smartedu-db', {
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('DB Connected Successfuly')
+});
 
 //template engine
 app.set("view engine", "ejs");
 
 //middlewares
 app.use(express.static("public"));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 //routes
 app.use('/', pageRoute);
+app.use('/courses', courseRoute);
 
 const port = 3000;
 app.listen(port, () => {
