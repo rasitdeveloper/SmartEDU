ae75acbb0c6a87bc3fc2ebf6dd6a77e678f6e3d5
rasitdeveloper <frasitkilic@gmail.com>
2022-04-24 14:50:19 +0300

    add dashboard

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const session = require('express-session');
+const MongoStore = require('connect-mongo');
 const pageRoute = require('./routes/pageRoute');
 const courseRoute = require('./routes/courseRoute');
 const categoryRoute = require('./routes/categoryRoute');
@@ -28,7 +29,8 @@ app.use(express.urlencoded({ extended: true }))
 app.use(session({
   secret: 'my_keyboard_cat',
   resave: false,
-  saveUninitialized: true
+  saveUninitialized: true,
+  store: MongoStore.create({mongoUrl: 'mongodb://localhost/smartedu-db'})
 }));
 
 
--- controllers/authController.js
+++ controllers/authController.js
@@ -26,7 +26,7 @@ exports.loginUser = async (req, res) => {
         if(same) {
           // User Session
           req.session.userID = user._id;
-          res.status(200).redirect('/');
+          res.status(200).redirect('/users/dashboard');
         }
       })
     }
@@ -49,3 +49,17 @@ exports.loginUser = async (req, res) => {
     });
   }
 };
+
+exports.logoutUser = (req,res) => {
+  req.session.destroy(() => {
+    res.redirect('/');
+  })
+}
+
+exports.getDashboardPage = async (req,res) => {
+  const user = await User.findOne({_id:req.session.userID});
+  res.status(200).render('dashboard', {
+    page_name: 'dashboard',
+    user
+  })
+}
