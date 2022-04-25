2c576fe2ef55664b9dc46b108ec6c528efca7b99
rasitdeveloper <frasitkilic@gmail.com>
2022-04-25 18:38:19 +0300

    security fixes added

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt');
+const { validationResult } = require('express-validator');
 const User = require('../models/User');
 const Category = require('../models/Category');
 const Course = require('../models/Course');
@@ -8,10 +9,11 @@ exports.createUser = async (req, res) => {
   const user = await User.create(req.body);
     res.status(201).redirect('/login');
   } catch (error) {
-    res.status(400).json({
-      status: 'fail',
-      error,
-    });
+    const errors = validationResult(req);
+    for(let i = 0; i < errors.array().length; i++) {
+      req.flash("error", `${errors.array()[i].msg}`);
+    }
+    res.status(201).redirect('/register');
   }
 };
 
@@ -22,10 +24,18 @@ exports.loginUser = async (req, res) => {
     const user = await User.findOne({email})
     if(user) {
       bcrypt.compare(password, user.password, (err, same) => {
+        if(same) {
           // User Session
           req.session.userID = user._id;
           res.status(200).redirect('/users/dashboard');
-      })
+        } else {
+          req.flash("error", "Your password is not correct!");
+          res.status(200).redirect('/login');
+        }
+      });
+    } else {
+      req.flash("error", "User is not exist!");
+      res.status(200).redirect('/login');
     }
     /*
     await User.findOne({email}, (err,user) => {
